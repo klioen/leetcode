9bd1984a275bc1e6d0d5741626c3fefd0d9524e9
klioen <605726828@qq.com>
2016-03-14 00:03:22 +0800

    refine

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,22 +1,17 @@
-c class Solution {
+public class Solution {
     public int reverse(int x) {
         int[] temp = new int[10]; 
         int i=0;
-        while(true) {
-            temp[i] = x%10;
-            x = x/10;
-            if(x == 0)
-                break;
-            i++;
-        }
         long result = 0;
+        while(x != 0) {
+            int t = x%10;
+            result = result*10 + t;
+            x = x/10;
+        }
+
         int flag = 1;
         if(x < 0) 
-            flag = -1;
-        for(int j=0; j<=i; j++) {
-            result += temp[j] * Math.pow(10, i-j);
-        }
-        result = result * flag;
+        result = -result;
         
         if(result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
             result = 0;
