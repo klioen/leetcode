2309549339f07ed0d1b255f12e708c84ae0261d3
klioen <605726828@qq.com>
2016-03-24 20:36:09 +0800

    add #27 easy

---
+++ RemoveElement.java
@@ -0,0 +1,13 @@
+c class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        int j = 0;
+        for(int i=0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                count++;
+                nums[j++] = nums[i];
+            }
+        }
+        return count;
+    }
+}
