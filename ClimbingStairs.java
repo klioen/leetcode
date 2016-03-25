17fccf2ea971af2d4894c0f75a9a59e904cbccc7
klioen <605726828@qq.com>
2016-03-25 22:41:02 +0800

    add #70 easy

---
+++ ClimbingStairs.java
@@ -0,0 +1,23 @@
+c class Solution {
+    
+    public int climbStairs(int n) {
+        int[] arr = new int[n+1];
+        if(n <= 3)
+            return n;
+        arr[1] = 1;
+        arr[2] = 2;
+        for(int i = 3; i <= n; i++)
+            arr[i] = arr[i-1] + arr[i-2];
+        return arr[n];
+    }
+    
+    
+    //Time Limit Exceeded
+    public int climbStairs1(int n) {
+        if(n <= 3)
+            return n;
+        else
+            return climbStairs(n-1) + climbStairs(n-2);
+            
+    }
+}
